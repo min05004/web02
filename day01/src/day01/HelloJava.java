273cac237ef570a1db129b0a694dc4b09d1e94e8
min05004 <139549560+min05004@users.noreply.github.com>
2023-12-01 15:09:25 +0900

    java

---
+++ day01/src/day01/HelloJava.java
@@ -0,0 +1,28 @@
+package day01;
+/*
+ * 데이터 타입
+ * 	정수 : byte, short, int(기본형), long
+ * 	실수 : float, double(표준)
+ * 	문자 : char ; 한 글자 ''로 표현
+ * 	문자열 : String ; 문장  "" 로 표현 - class형태
+ * 	논리형 : boolean ; true,false 로 표현.
+ * 	
+ *  변수 선언
+ *  데이터타입 이름 = 값; -> 초기값
+ *  데이터타입 이름; -> boolean 
+ *  
+ * 
+ */
+public class HelloJava { // 클래스 네이밍 첫글자는 대문자!
+
+	public static void main(String[] args) {
+		// 자바프로그램이 최초로 실행하는 메인 메서드
+		System.out.println("안녕하세요");
+	
+		
+		
+	
+
+	}
+
+}
